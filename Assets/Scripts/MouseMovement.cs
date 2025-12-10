0e7b931ec223b12ee352cacd295f1b574b2f0160
KarlosEder <167484213+KarlosEder@users.noreply.github.com>
2025-12-10 20:01:56 +1300

    Basic player movement, and HUD.
    General Git setup, created basic player movement, map, and HUD.

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MouseMovement : MonoBehaviour
+{
+
+    public float mouseSensitivity = 500f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // X axis rotation 
+        xRotation -= mouseY;
+
+        // Clamp rotation
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        // Y axis rotation 
+        yRotation += mouseX;
+
+        // Apply rotations to transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
